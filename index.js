860fec8d00459cc0a555332b29b193dec2f1a962
Leximiller128 <leximiller128@gmail.com>
2022-08-23 18:34:28 -0600

    added files, started code in test.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import Engineer from "./lib/Engineer.cjs";
+import Intern from "./lib/Intern.cjs";
+import Manager from "./lib/Manager.cjs";
+
+import inquirer from "inquirer";
+console.log("hello world");
